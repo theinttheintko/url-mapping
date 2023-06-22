984b8325a102bb6c402ced6abd8291db6f0c0b8b
theinttheintko <theinttheintko26@gmail.com>
2023-06-21 21:57:31 -0700

    url-mapping

---
+++ src/main/java/com/example/urlmappingdemo/controller/WelcomeController.java
@@ -0,0 +1,29 @@
+package com.example.urlmappingdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+    @GetMapping("/dob")
+    public String DateOfBirth(Model model){
+        model.addAttribute("tagline","Happy Birthday");
+        return "welcome";
+    }
+    @GetMapping("/greet")
+    public String greeting(Model model){
+        model.addAttribute("tagline","Hello");
+        return "welcome";
+    }
+    @GetMapping("/sayspring")
+    public String saySpring(Model model){
+        model.addAttribute("tagline","Say Spring MVC");
+        return "welcome";
+    }
+    @GetMapping({"/","/home"})
+    public String welcome1(Model model){
+        model.addAttribute("tagline","Welcome Spring MVC");
+        return "welcome";
+    }
+}
